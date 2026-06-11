8bc9803597b549c4dddacb28efe371b910696c1e
Saneeshkp85 <saneesh@netconnectdigital.com>
2026-06-11 19:29:43 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  trailingSlash: true,
-
   images: {
-    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "shop.nalalifeline.org" },
       { protocol: "https", hostname: "nalalifeline.org" },
